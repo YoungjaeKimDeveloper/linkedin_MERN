6492c44d2cf760ccb2bab3e9a0ddf624ba0602d0
Youngjae <2015fromjeki@gmail.com>
2025-01-20 19:51:43 +1100

    Start USer Routes

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT;
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-// Routes 마
+// Routes
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
